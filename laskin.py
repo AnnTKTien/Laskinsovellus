508aa59d3339d56f94d17e65dc5c0e6666222ee7
AnnTKTien <annukka.tienari@gmail.com>
2026-03-06 15:11:45 +0200

    Update laskin.py

--- laskin.py
+++ laskin.py
@@ -28,18 +28,18 @@ def div(a, b):
     return a / b
 
 # Tämä on tulostus, joka suoritetaan ohjelman alussa
-print("""---Laskin--
-Valitse lasku operaatio
-1.Summa
-2.Erotus
-3.Tulo
-4.Jako""")
+print("""\n---Laskin--\n
+-Valitse lasku operaatio-
+1. Summa
+2. Erotus
+3. Tulo
+4. Jako\n""")
 
-# While-loopin sisällä varmistetaan että valittu arvo on välillä (1-4) vain numeeriset arvot hyväksytään, mutta arvot vastaanotetaan string-muodossa.
+# While-loopin sisällä varmistetaan että valittu arvo on välillä (1-4) vain numeeriset arvot hyväksytään, mutta vastaanotetaan tekstinä.
 while True:
     valinta = input("Valitse operaatio numeroilla (1-4): ")
-# Jos valinta on välillä (1-4) suoritetaan funktio valinnan mukaan, tapahtuu tulostus ja sen jälkeen ohjelman suoritus lopetetaan.
-#Valinnoissa myös muuttuja merkille, joka määrittää mikä laskun mukana tulostettaan.
+# Jos valinta on välillä (1-4) suoritetaan funktio valinnan mukaan ja sen jälkeen ohjelman suoritus lopetetaan. Valinnoissa myös muuttuja merkille, joka määrittää mikä
+# laskun mukana tulostettaan.
     if valinta in ["1", "2", "3", "4"]:
         # Pyydetään käyttäjältä kaksi arvoa
         a = int(input("Anna ensimmäinen numero: "))
